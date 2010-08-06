cf9302ad1519d1d4221196b2865c5cd50e9000ad
Firehed <firehed@gmail.com>
2010-08-06 00:04:10 -0700

    Handle Sec-WebSocket-Key stuff, strip frame padding bytes

--- app/Client.php
+++ app/Client.php
@@ -25,8 +25,8 @@ class Client {
 			$this->disconnect();
 			return;
 		}
-		$input = trim($input);
-		
+		$input = trim($input, "\x00\xFF");
+
 		if (self::State_Connected != $this->state) {
 			$this->handleLogin($input);
 			return;
@@ -36,16 +36,35 @@ class Client {
 		
 	} // function handleInput
 	
-	public function handleLogin($input) {
-		$r=$h=$o=null;
-		if(preg_match("/GET (.*) HTTP/",        $input,$match)){ $r=trim($match[1]); }
-		if(preg_match("/Host: (.*)(\n|\r|$)/",  $input,$match)){ $h=trim($match[1]); }
-		if(preg_match("/Origin: (.*)(\n|\r|$)/",$input,$match)){ $o=trim($match[1]); }
+	public function handleLogin($in) {
+		$request = $host = $origin = $key1 = $key2 = $verifier = $response = $sec = '';
+		
+		if (preg_match('/GET (.*) HTTP/',                    $in,$m)) $request  = $m[1];
+		if (preg_match('/Host: (.*)(\r\n|$)/',               $in,$m)) $host     = $m[1];
+		if (preg_match('/Origin: (.*)(\r\n|$)/',             $in,$m)) $origin   = $m[1];
+		if (preg_match('/Sec-WebSocket-Key1: (.*)(\r\n|$)/', $in,$m)) $key1     = $m[1];
+		if (preg_match('/Sec-WebSocket-Key2: (.*)(\r\n|$)/', $in,$m)) $key2     = $m[1];
+		if (preg_match('/\r\n\r\n(.*)$/',                    $in,$m)) $verifier = $m[1];
+		
+		// Not all clients send the Sec-WebSocket stuff, so only build out the response if a challenge was provided
+		if ($key1 && $key2 && $verifier) {
+			$digits1 = preg_replace('/[^0-9]/', '', $key1);
+			$digits2 = preg_replace('/[^0-9]/', '', $key2);
+			$spaces1 = substr_count($key1, ' ');
+			$spaces2 = substr_count($key2, ' ');
+			
+			$cat = pack('N',$digits1/$spaces1) . pack('N', $digits2/$spaces2) . $verifier;
+			
+			$response = "\r\n" . md5($cat, TRUE);
+			$sec = 'Sec-';
+		}
+		
 		$upgrade  = "HTTP/1.1 101 Web Socket Protocol Handshake\r\n"
 		          . "Upgrade: WebSocket\r\n"
 		          . "Connection: Upgrade\r\n"
-		          . "WebSocket-Origin: $o\r\n"
-		          . "WebSocket-Location: ws://$h$r\r\n"
+		          . "{$sec}WebSocket-Origin: $origin\r\n"
+		          . "{$sec}WebSocket-Location: ws://$host$request\r\n"
+		          . $response
 		          . "\r\n";
 		socket_write($this->socket, $upgrade);
 		$this->state = self::State_Connected;
