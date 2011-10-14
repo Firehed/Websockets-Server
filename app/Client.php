bced022cb3eff787fad1c9b6694ccafc00567c1b
Eric Stern <firehed@gmail.com>
2011-10-14 15:46:29 -0700

    start work on v13 of the protocol, now in Chrome canary and others

--- app/Client.php
+++ app/Client.php
@@ -4,6 +4,8 @@ class Client {
 	const State_New       = 1; // Just connected
 	const State_Connected = 2;
 	
+	const Guid_v13 = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11';
+
 	private static $count = 0;
 	private $socket;
 	private $position;
@@ -26,6 +28,8 @@ class Client {
 			return;
 		}
 		$input = trim($input, "\x00\xFF");
+		echo "Message Received:\n";
+		echo $input;
 
 		if (self::State_Connected != $this->state) {
 			$this->handleLogin($input);
@@ -37,28 +41,55 @@ class Client {
 	} // function handleInput
 	
 	public function handleLogin($in) {
-		$request = $host = $origin = $key1 = $key2 = $verifier = $response = $sec = '';
-		
-		if (preg_match('/GET (.*) HTTP/',                    $in,$m)) $request  = $m[1];
-		if (preg_match('/Host: (.*)(\r\n|$)/',               $in,$m)) $host     = $m[1];
-		if (preg_match('/Origin: (.*)(\r\n|$)/',             $in,$m)) $origin   = $m[1];
-		if (preg_match('/Sec-WebSocket-Key1: (.*)(\r\n|$)/', $in,$m)) $key1     = $m[1];
-		if (preg_match('/Sec-WebSocket-Key2: (.*)(\r\n|$)/', $in,$m)) $key2     = $m[1];
-		if (preg_match('/\r\n\r\n(.*)$/',                    $in,$m)) $verifier = $m[1];
-		
-		// Not all clients send the Sec-WebSocket stuff, so only build out the response if a challenge was provided
-		if ($key1 && $key2 && $verifier) {
-			$digits1 = preg_replace('/[^0-9]/', '', $key1);
-			$digits2 = preg_replace('/[^0-9]/', '', $key2);
-			$spaces1 = substr_count($key1, ' ');
-			$spaces2 = substr_count($key2, ' ');
-			
-			$cat = pack('N',$digits1/$spaces1) . pack('N', $digits2/$spaces2) . $verifier;
-			
-			$response = "\r\n" . md5($cat, TRUE);
-			$sec = 'Sec-';
+		/*
+		GET / HTTP/1.1
+		Upgrade: websocket
+		Connection: Upgrade
+		Host: 127.0.0.1:9000
+		Origin: https://vm.wepay.com
+		Sec-WebSocket-Key: iguoNlFsLEeeK2D+t90RMg==
+		Sec-WebSocket-Version: 13
+		*/
+		// $request = $host = $origin = $key1 = $key2 = $verifier = $response = $sec = '';
+		$request = $upgrade = $connection = $host = $origin = $key = $version = '';
+		if (preg_match('/GET (.*) HTTP/',                       $in,$m)) $request    = $m[1];
+		if (preg_match('/Upgrade: (.*)(\r\n|$)/',               $in,$m)) $upgrade    = $m[1];
+		if (preg_match('/Connection: (.*)(\r\n|$)/',            $in,$m)) $connection = $m[1];
+		if (preg_match('/Host: (.*)(\r\n|$)/',                  $in,$m)) $host       = $m[1];
+		if (preg_match('/Origin: (.*)(\r\n|$)/',                $in,$m)) $origin     = $m[1];
+		if (preg_match('/Sec-WebSocket-Key: (.*)(\r\n|$)/',     $in,$m)) $key        = $m[1];
+		if (preg_match('/Sec-WebSocket-Version: (.*)(\r\n|$)/', $in,$m)) $version    = $m[1];
+		unset($m);
+
+		if (!$key) {
+			$this->rejectConnection();
 		}
+		if ($version != 13) {
+			$this->rejectConnection();
+		}
+		// Authenticate against the key
+
+		$auth = self::authenticateKey($key);
+		$upgrade = "HTTP/1.1 101 Switching Protocols\r\n"
+		. "Upgrade: websocket\r\n"
+		. "Connection: Upgrade\r\n"
+		. "Sec-WebSocket-Accept: $auth\r\n"
+		. "\r\n";
 		
+		echo $upgrade;
+		// 
+		// if ($key1 && $key2 && $verifier) {
+		// 	$digits1 = preg_replace('/[^0-9]/', '', $key1);
+		// 	$digits2 = preg_replace('/[^0-9]/', '', $key2);
+		// 	$spaces1 = substr_count($key1, ' ');
+		// 	$spaces2 = substr_count($key2, ' ');
+		// 	
+		// 	$cat = pack('N',$digits1/$spaces1) . pack('N', $digits2/$spaces2) . $verifier;
+		// 	
+		// 	$response = "\r\n" . md5($cat, TRUE);
+		// 	$sec = 'Sec-';
+		// }
+		/*
 		$upgrade  = "HTTP/1.1 101 Web Socket Protocol Handshake\r\n"
 		          . "Upgrade: WebSocket\r\n"
 		          . "Connection: Upgrade\r\n"
@@ -66,9 +97,14 @@ class Client {
 		          . "{$sec}WebSocket-Location: ws://$host$request\r\n"
 		          . $response
 		          . "\r\n";
+		*/
 		socket_write($this->socket, $upgrade);
 		$this->state = self::State_Connected;
 	} // function handleLogin
+
+	private static function authenticateKey($key) {
+		return base64_encode(sha1($key . self::Guid_v13, true));
+	}
 	
 	public function getSocket() {
 		return $this->socket;
@@ -79,8 +115,13 @@ class Client {
 	} // function getPosition
 	
 	public function disconnect() {
+		echo 'Disconnected';
+		
 		socket_close($this->socket);
 		Server::removeClient($this);
 	} // function disconnect
 	
+	public function rejectConnection() {
+		// implement me
+	} // function rejectConnection
 } // class Client
